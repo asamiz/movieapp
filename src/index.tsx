4782fa0791027c850cc84624b82d55f9a3541252
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2020-09-16 22:01:29 +0200

    :sparkles: Add native splash screen for IOS & Android

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+ View, Text, StyleSheet, StatusBar,
+} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { COLORS, FONTS } from './common';
 
-const App: React.FC<{}> = () => (
-	<View style={styles.container}>
-		<Text style={styles.text}>{'Movie App'}</Text>
-	</View>
-);
+const App: React.FC<{}> = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			SplashScreen.hide();
+		}, 500);
+	}, []);
+
+	return (
+		<>
+			<StatusBar
+				backgroundColor={COLORS.oxfordBlue}
+				barStyle={'light-content'}
+			/>
+			<View style={styles.container}>
+				<Text style={styles.text}>{'Movie App'}</Text>
+			</View>
+		</>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.oxfordBlue,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontFamily: FONTS.bold,
-    color: COLORS.aquamarine,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: COLORS.white,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		fontFamily: FONTS.bold,
+		color: COLORS.oxfordBlue,
+	},
 });
 
 export default App;
