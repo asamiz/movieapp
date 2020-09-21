23019a061f1634b19919889aa159645bb0097b4f
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2020-09-21 17:05:37 +0200

    :lipstick: Minor fix in RatingList component

--- src/components/RatingList/styles.ts
+++ src/components/RatingList/styles.ts
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: COLORS.spaceCadet,
+		flexGrow: 1,
 	},
 	source: {
 		fontFamily: FONTS.bold,
